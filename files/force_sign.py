d4cca0988e730530e59c9e8c4f6e9bfd9a89e0e7
Xand Meaden <xand.meaden@kcl.ac.uk>
2019-09-09 18:25:11 +0100

    wip

--- files/force_sign.py
+++ files/force_sign.py
@@ -2,3 +2,36 @@
 #
 # Reject (or warn) pushes of unsigned commits to a GitLab repository
 # Part of puppet-gitlab_gpg - https://github.com/kcl-nmssys/puppet-gitlab_gpg
+
+import os
+import subprocess
+import sys
+import syslog
+import yaml
+
+try:
+    with open('/etc/gitlab_gpg/config.yaml') as fh:
+        config = yaml.load(fh, Loader=yaml.SafeLoader)
+except:
+    sys.stderr.write('Failed to load configuration files\n')
+    sys.exit(1)
+
+branch = sys.argv[1]
+rev_old = sys.argv[2]
+rev_new = sys.argv[3]
+
+# Special 'hash' used when there are no previous commits
+zeroes = '0000000000000000000000000000000000000000'
+
+if rev_old == zeroes:
+    git_rev_args = [rev_new, '--not', '--branches=*']
+else:
+    git_rev_args = ['%s..%s' % (rev_old, rev_new)]
+
+try:
+    revs = subprocess.check_output([config['git_path'], 'rev-list'] + git_rev_args).rstrip().split('\n')
+except:
+    sys.stderr.write('Failed to get commit list\n')
+    sys.exit(1)
+
+print(revs)
