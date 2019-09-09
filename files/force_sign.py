86336727df391c40f18c2216897450cd2ee4d0c6
Xand Meaden <xand.meaden@kcl.ac.uk>
2019-09-09 20:40:36 +0100

    wip2

--- files/force_sign.py
+++ files/force_sign.py
@@ -9,16 +9,28 @@ import sys
 import syslog
 import yaml
 
+
 try:
     with open('/etc/gitlab_gpg/config.yaml') as fh:
         config = yaml.load(fh, Loader=yaml.SafeLoader)
 except:
-    sys.stderr.write('Failed to load configuration files\n')
+    sys.stderr.write('Failed to load main configuration file\n')
     sys.exit(1)
+
+cwd = os.getcwd()
+if cwd.startswith(config['repos_path']):
+    group_project = cwd[len(config['repos_path']):]
+    try:
+        with open('/etc/gitlab_gpg/repos/%s.yaml') as fh:
+            config = config + yaml.load(fh, Loader=yaml.SafeLoader)
+    except:
+        sys.stderr.write('Failed to load specific configuration file\n')
+        sys.exit(1)
 
 branch = sys.argv[1]
 rev_old = sys.argv[2]
 rev_new = sys.argv[3]
+username = os.environ['GL_USERNAME']
 
 # Special 'hash' used when there are no previous commits
 zeroes = '0000000000000000000000000000000000000000'
@@ -29,9 +41,12 @@ else:
     git_rev_args = ['%s..%s' % (rev_old, rev_new)]
 
 try:
-    revs = subprocess.check_output([config['git_path'], 'rev-list'] + git_rev_args).rstrip().split('\n')
+    commits = subprocess.check_output([config['git_path'], 'rev-list'] + git_rev_args).rstrip().split('\n')
 except:
     sys.stderr.write('Failed to get commit list\n')
     sys.exit(1)
+
+for commit in commits:
+
 
 print(revs)
