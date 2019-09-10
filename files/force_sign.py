fcd84202de706dc483bbc6e8e63e45f7999ac4e2
Xand Meaden <xand.meaden@kcl.ac.uk>
2019-09-10 16:59:56 +0100

    do checking

--- files/force_sign.py
+++ files/force_sign.py
@@ -3,12 +3,20 @@
 # Reject (or warn) pushes of unsigned commits to a GitLab repository
 # Part of puppet-gitlab_gpg - https://github.com/kcl-nmssys/puppet-gitlab_gpg
 
+import datetime
 import os
 import subprocess
 import sys
 import syslog
+import time
 import yaml
 
+def git_show(commit, format):
+    try:
+        output = subprocess.check_output([config['git_path'], 'show', commit, '-s', '--format=%%%s' % format])
+    except:
+        sys.stderr.write('Failed to get git data %s for commit %s' % (format, commit))
+    return output.strip()
 
 try:
     with open('/etc/gitlab_gpg/config.yaml') as fh:
@@ -19,10 +27,10 @@ except:
 
 cwd = os.getcwd()
 if cwd.startswith(config['repos_path']):
-    group_project = cwd[len(config['repos_path']):]
+    group_project = cwd[len(config['repos_path']) + 1:-4]
     try:
-        with open('/etc/gitlab_gpg/repos/%s.yaml') as fh:
-            config = config + yaml.load(fh, Loader=yaml.SafeLoader)
+        with open('/etc/gitlab_gpg/repos/%s.yaml' % group_project) as fh:
+            config.update(yaml.load(fh, Loader=yaml.SafeLoader))
     except:
         sys.stderr.write('Failed to load specific configuration file\n')
         sys.exit(1)
@@ -46,7 +54,32 @@ except:
     sys.stderr.write('Failed to get commit list\n')
     sys.exit(1)
 
+exit_status = 0
+
+commit_meta = {}
+sig_status = {
+    'G': 'good signature',
+    'U': 'good signature, unknown validity',
+    'X': 'good signature, expired',
+    'Y': 'good signature, expired key',
+    'R': 'good signature, revoked key',
+    'B': 'bad signature',
+    'E': 'cannot check',
+    'N': 'not signed',
+}
+fields = {
+    'author_email': 'ae',
+    'author_name': 'an',
+    'committer_email': 'ce',
+    'committer_name': 'cn',
+    'timestamp': 'ct',
+    'comment': 'B',
+    'sig_status': 'G?'
+}
 for commit in commits:
-
-
-print(revs)
+    commit_meta[commit] = {}
+    for field_name in fields.keys():
+        commit_meta[commit][field_name] = git_show(commit, fields[field_name])
+    if commit_meta['sig_status'] in ['B', 'E', 'N', 'R']:
+        if config['ensure'] == 'protected':
+            exit_status = 1
