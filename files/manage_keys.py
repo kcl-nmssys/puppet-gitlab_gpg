acce807f508493d5f12ffcdc69aa7678b69fd3c9
Xand Meaden <xand.meaden@kcl.ac.uk>
2020-02-03 15:13:11 +0000

    manage keys in gpg keyring and gitlab

--- files/manage_keys.py
+++ files/manage_keys.py
@@ -52,7 +52,7 @@ for user in users:
                 fh.write('%s\n' % key.key)
 
             key_data = gpg.scan_keys(key_file)[0]
-            current_keys_gitlab[key_data['fingerprint']] = 1
+            current_keys_gitlab[key_data['fingerprint']] = {'user': user.username, 'key': key.id}
 
 # Import any trusted keys
 for file in os.listdir('keys/trusted'):
@@ -62,6 +62,7 @@ for file in os.listdir('keys/trusted'):
         key_path = 'keys/trusted/%s' % file
         keys_data = gpg.scan_keys(key_path)
         for key_data in keys_data:
+            trusted[key_data['fingerprint']] = 1
             if key_data['fingerprint'] not in current_keys_keyring:
                 if args.mode == 'check':
                     need_update = True
@@ -69,9 +70,9 @@ for file in os.listdir('keys/trusted'):
                     with open(key_path) as fh:
                         import_result = gpg.import_keys(fh.read())
                     if import_result.count > 0:
-                        syslog.syslog(syslog.LOG_INFO, 'Imported key [%s] [%s]' % (key_data['fingerprint'], ', '.join(key_data['uids'])))
+                        syslog.syslog(syslog.LOG_INFO, 'Imported key [%s] [%s] into keyring' % (key_data['fingerprint'], ', '.join(key_data['uids'])))
                     else:
-                        syslog.syslog(syslog.LOG_ERR, 'Failed importing key [%s] [%s]' % (key_data['fingerprint'], ', '.join(key_data['uids'])))
+                        syslog.syslog(syslog.LOG_ERR, 'Failed importing key [%s] [%s] into keyring' % (key_data['fingerprint'], ', '.join(key_data['uids'])))
                         import_status = 1
 
             if key_data['fingerprint'] not in current_keys_gitlab:
@@ -80,9 +81,24 @@ for file in os.listdir('keys/trusted'):
                 else:
                     user = gl.users.list(username=username)[0]
                     user.gpgkeys.create({'key': gpg.export_keys(key_data['fingerprint'])})
+                    syslog.syslog(syslog.LOG_INFO, 'Imported key [%s] [%s] into GitLab' % (key_data['fingerprint'], username))
 
-# Remove any unexpected keys
-nyi
+# Remove any unexpected keys from GitLab
+if config['manage_gitlab_keys']:
+    for fingerprint in current_keys_gitlab.keys():
+        if fingerprint not in trusted:
+            if args.mode == 'check':
+                need_update = True
+            else:
+                user = gl.users.list(username=current_keys_gitlab[fingerprint][user])[0]
+                user.gpgkeys.delete(current_keys_gitlab[fingerprint][key])
+                syslog.syslog(syslog.LOG_INFO, 'Deleted key [%s] [%s] from GitLab' % (fingerprint, current_keys_gitlab[fingerprint][user]))
+
+# Remove any unexpected keys from keyring
+for fingerprint in current_keys_keyring:
+    if fingerprint not in trusted:
+        gpg.delete_keys(fingerprint)
+        syslog.syslog(syslog.LOG_INFO, 'Deleted key [%s] from keyring' % (fingerprint,))
 
 if args.mode == 'check':
     if need_update:
