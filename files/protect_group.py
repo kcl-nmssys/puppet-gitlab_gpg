55a95baabc1810d040c50b04c0789d0803b910bc
Xand Meaden <xand.meaden@kcl.ac.uk>
2020-02-04 10:29:58 +0000

    fixes to python code

--- files/protect_group.py
+++ files/protect_group.py
@@ -7,6 +7,7 @@ import argparse
 import os
 import re
 import sys
+import yaml
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--mode')
@@ -26,11 +27,11 @@ if not args.ensure or args.ensure not in ['absent', 'present']:
 try:
     with open('/etc/gitlab_gpg/config.yaml') as fh:
         config = yaml.load(fh, Loader=yaml.SafeLoader)
-except:
-    sys.stderr.write('Failed to load configuration file\n')
+except Exception as e:
+    sys.stderr.write('Failed to load configuration file: %s\n' % (e,))
     sys.exit(1)
 
-hook_link_src = '%/force_sign.py' % (config['install_path'],)
+hook_link_src = '%s/force_sign.py' % (config['install_path'],)
 
 group_dir = '%s/%s' % (config['repos_path'], args.group)
 if not os.path.exists(group_dir):
@@ -38,47 +39,46 @@ if not os.path.exists(group_dir):
     sys.exit(1)
 
 need_update = 0
-with os.scandir(group_dir) as dh_g:
-    for entry in dh_g:
-        if re.match(r'^([a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_])\.git$', entry.name) and not re.match(r'^([a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_])\.wiki\.git$', entry.name):
-            hook_dir = '%s/custom_hooks' % (group_dir,)
-            hook_link = '%s/update' % (hook_dir,)
+for entry in os.listdir(group_dir):
+    if re.match(r'^([a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_])\.git$', entry) and not re.match(r'^([a-zA-Z0-9_\.][a-zA-Z0-9_\-\.]*[a-zA-Z0-9_\-]|[a-zA-Z0-9_])\.wiki\.git$', entry):
+        hook_dir = '%s/custom_hooks' % (group_dir,)
+        hook_link = '%s/update' % (hook_dir,)
 
-            if args.ensure == 'present':
-                if os.path.isdir(hook_dir):
-                    if not (os.path.islink(hook_link) and os.readlink(hook_link == hook_link_src)):
-                        need_update += 1
-                        if args.mode == 'update':
-                            if os.path.exists(hook_link):
-                                try:
-                                    os.unlink(hook_link)
-                                except Exception as e:
-                                    sys.stderr.write('Failed deleting incorrect custom_hooks entity %s: %s\n' % (hook_link, e))
-                                    sys.exit(1)
-                            try:
-                                os.symlink(hook_link_src, hook_link)
-                            except Exception as e:
-                                sys.stderr.write('Failed creating symlink %s: %s\n' % (hook_link, e))
-                                sys.exit(1)
-
-                else:
+        if args.ensure == 'present':
+            if os.path.isdir(hook_dir):
+                if not (os.path.islink(hook_link) and os.readlink(hook_link == hook_link_src)):
                     need_update += 1
                     if args.mode == 'update':
+                        if os.path.exists(hook_link):
+                            try:
+                                os.unlink(hook_link)
+                            except Exception as e:
+                                sys.stderr.write('Failed deleting incorrect custom_hooks entity %s: %s\n' % (hook_link, e))
+                                sys.exit(1)
                         try:
-                            os.mkdir(hook_dir, 0755)
                             os.symlink(hook_link_src, hook_link)
                         except Exception as e:
-                            sys.stderr.write('Failed creating directory and symlink %s: %s\n' % (hook_link, e))
+                            sys.stderr.write('Failed creating symlink %s: %s\n' % (hook_link, e))
                             sys.exit(1)
+
             else:
-                if os.path.islink(hook_link) and os.readlink(hook_link == hook_link_src):
-                    need_update += 1
-                    if args.mode == 'update':
-                        try:
-                            os.unlink(hook_link)
-                        except Exception as e:
-                            sys.stderr.write('Failed to remove link %s: %s\n' % (hook_link, e))
-                            sys.exit(1)
+                need_update += 1
+                if args.mode == 'update':
+                    try:
+                        os.mkdir(hook_dir, 0755)
+                        os.symlink(hook_link_src, hook_link)
+                    except Exception as e:
+                        sys.stderr.write('Failed creating directory and symlink %s: %s\n' % (hook_link, e))
+                        sys.exit(1)
+        else:
+            if os.path.islink(hook_link) and os.readlink(hook_link == hook_link_src):
+                need_update += 1
+                if args.mode == 'update':
+                    try:
+                        os.unlink(hook_link)
+                    except Exception as e:
+                        sys.stderr.write('Failed to remove link %s: %s\n' % (hook_link, e))
+                        sys.exit(1)
 
 if args.mode == 'update':
     sys.exit(need_update)
